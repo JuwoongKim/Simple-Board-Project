29d7d2202541c90abfe57b3750649051272f486c
juwoong <wwong7247@gmail.com>
2022-10-29 16:03:40 +0900

    #8-스프링부트로 프로젝트 생성
    의존성 
    -스프링 웹
    -롬북
    -데브툴즈
    -엑츄에이터

---
+++ src/test/java/com/example/simpleboardproject/SimpleBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleboardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
